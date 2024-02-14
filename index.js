1d528597b3bb43ca09cd118ca4640863480ff5f3
Fabien Fiette <fabienfiette@hotmail.fr>
2024-02-14 14:25:28 +0100

    feat: MESSSIIIII

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function toto() {
   return "Hello My name is toto"
 }
 
+function paraMessi() {
+  return "Para Messi, para Messi, para Messi... MESSSSIIII!"
+}
+
 module.exports = {
   toto,
+  paraMessi
 }
